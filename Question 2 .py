d65cb31b24a8411ba3ea49f4fe64de2bf9fc2c84
1235735 <myleskahura@gmail.com>
2025-02-08 10:35:20 +0300

    Create Question 2 .py

---
+++ Question 2 .py
@@ -0,0 +1,9 @@
+speed = input("whats your speed? ")
+
+if int (speed) > 25:
+    print("your a super hero")
+elif int (speed) == 25:
+    print("you just made it!")
+else:
+    print ("keep training!")
+    
